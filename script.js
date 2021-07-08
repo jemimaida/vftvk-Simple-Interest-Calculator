60a540a6f8b95a71b428e2068c43e4c53dfe3bec
jemimaida <59909457+jemimaida@users.noreply.github.com>
2021-07-08 12:13:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-
 function compute()
 {
-    if(principal.value < 1){
-        alert("Amount has to be a positive quantity")
-        principal.focus()
-        return
-    }
-    let interest = principal.value * years.value * rate.value / 100;
-    yearInFuture = new Date().getFullYear() + parseInt(years.value);
-    result = document.getElementById("result")
-    result.innerHTML = "<p>If you deposit <mark>"+ principal.value +"</mark>,<br>an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+yearInFuture+"</mark></p>";
+    var principal = document.getElementById("principal").value;
 
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+    
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
 
-function refreshSlider(){
-rate.value
-document.getElementById("rateLabel").innerText = rate.value + "%"}
+function SliderValue()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate_display");
+    output.innerHTML = slider.value; // Display the default slider value
 
-finishPage = () =>{
-    var rate = document.getElementById("rate");
-    var principal = document.getElementById("principal");
-    var years = document.getElementById("years");
-    years = document.getElementById("years");
-    for(i=1; i<=10; i++){
-    	let element = document.createElement("option")
-    	element.innerText = i
-    	element.setAttribute("value", i)
-    	years.appendChild(element)
-    }
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }  
 }
